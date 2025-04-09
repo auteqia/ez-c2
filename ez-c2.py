3e591142cbc8f03c38731614473bfb9ac2d61a98
auteqia <auteqia@auteqia.garden>
2025-04-09 14:24:23 +0200

    removed color class

--- ez-c2.py
+++ ez-c2.py
@@ -9,24 +9,6 @@ import json
 import ssl
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    CYAN = '\033[36m'
-    GREEN = '\033[32m'
-    RED = '\033[31m'
-    YELLOW = '\033[33m'
-    WHITE = '\033[37m'
-    BLACK = '\033[30m'
-    MAGENTA = '\033[35m'
-    BLUE = '\033[34m'
 
 def enumerate_system():
     """Capture system information and environment variables in a structured JSON format."""
